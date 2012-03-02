1cae2062df78d808bb33a0ef7ff87267cf02e477
Adam Newman <newman85@gmail.com>
2012-03-01 20:37:50 -0600

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Byob::Application.config.secret_token = '4627974a57d42a478094c71e8badf1b6e9008b4c959d4e84ae1823b50c39dd6c1cfeed6f26a3cbb4719ed345f5ae542fc6313dbfcf5e5657c935f8959068ac3e'
