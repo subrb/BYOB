2c462f9d7b1db29cb8df29492f94613873b6ebcf
Adam Newman <newman85@gmail.com>
2012-03-05 21:03:45 -0600

    lesson 2 conclusion

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ActiveRecord::Base
+  belongs_to :ethnic_group
+  accepts_nested_attributes_for :ethnic_group
+  
   validates :name, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120306020306) do
+ActiveRecord::Schema.define(:version => 20120306023428) do
+
+  create_table "ethnic_groups", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "restaurants", :force => true do |t|
     t.string   "name"
     t.boolean  "byob"
     t.integer  "price"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.integer  "ethnic_group_id"
   end
+
+  add_index "restaurants", ["ethnic_group_id"], :name => "index_restaurants_on_ethnic_group_id"
 
 end
